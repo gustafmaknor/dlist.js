f0623138d8b1a3cfc0b9478d03fd13b180466c24
Gustaf Mäkinen Nordquist <gustafmakinennordquist@gmail.com>
2013-06-27 22:54:13 +0200

    added file index.js as a work in progress

---
+++ index.js
@@ -0,0 +1,24 @@
+(function(g_ctx, undefined){
+	"use strict";
+var Index=function(comparer){
+	this.list=[];
+	this.length=0;
+	this.comparer=comparer;
+}
+Index.prototype.add=function(key, value){
+
+	
+	this.length++;
+}
+Index.prototype.search=function(key){
+
+
+}
+Index.prototype.remove=function(key){
+
+	this.length--;
+}
+
+
+
+})(this);
